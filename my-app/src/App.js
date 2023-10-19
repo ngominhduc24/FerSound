59c9e4fc89b6a56d13d0715b83d8dcbbfedcf61b
Quang Phạm <107977015+pmq2408@users.noreply.github.com>
2023-10-20 01:34:29 +0700

    login
    new by pmq 2408

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,9 +1,22 @@
-function App() {
-  return (
-    <div>
-      <h1>Hello, world!</h1>
-    </div>
-  );
-}
+import React from 'react';
+import Login from './components/Login/login';
 
+import LogoFerSound from './components/Login/img/logofersound.png';
+
+const App = () => {
+  return(
+    <div className="App">
+
+      <div className="fersound-logo">
+        <img src={LogoFerSound}/>
+      </div>
+
+      <div className="broke-line-top">
+        <hr className="src"></hr>
+      </div>
+
+      <Login/>
+    </div>
+  )
+};
 export default App;
