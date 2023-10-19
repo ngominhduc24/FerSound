39ac38cb8808b3bcf4dfbf6261fdb8d78667040d
duykhanh21052003 <khanhnguyenduy2105@gmail.com>
2023-10-20 01:12:12 +0700

    update page play music

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,7 +1,22 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { Col, Container, Row } from 'react-bootstrap'
+import PlayMusic from './PlayMusic';
 function App() {
   return (
     <div>
-      <h1>Hello, world!</h1>
+      <Container>
+        <BrowserRouter>
+          <Row>
+            <Col md={12}>
+            </Col>
+            <Col>
+              <Routes>
+                <Route path='/play' element={<PlayMusic />}></Route>
+              </Routes>
+            </Col>
+          </Row>
+        </BrowserRouter>
+      </Container>
     </div>
   );
 }
