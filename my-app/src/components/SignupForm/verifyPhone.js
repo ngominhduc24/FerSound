ebd137c82638654d294e779ac002801ee6498b6c
Minh Duc <ngominhduc100@gmail.com>
2023-11-10 01:04:52 +0700

    update front end verify phone

--- my-app/src/components/SignupForm/verifyPhone.js
+++ my-app/src/components/SignupForm/verifyPhone.js
@@ -1,12 +1,15 @@
 import React, { useEffect, useState } from 'react';
-import { firebase } from './firebase'; // Adjust the path based on your file structure
+import { firebase } from './firebase';
+import { Col, Row } from 'react-bootstrap';
+import logoImage from '../../Assets/logofersound.png';
+import { MDBBtn, MDBInput } from 'mdb-react-ui-kit';
 
 const VerifyPhoneComponent = ({ phoneNumber, step }) => {
     const [otp, setOtp] = useState('');
     const [verificationId, setVerificationId] = useState(null);
     const [recaptchaVerifier, setRecaptchaVerifier] = useState(null);
     const [loading, setLoading] = useState(true);
-    const [isCaptchaVisible, setIsCaptchaVisible] = useState(false);
+    const [isCaptchaVisible, setIsCaptchaVisible] = useState(true);
 
     useEffect(() => {
         const verifier = new firebase.auth.RecaptchaVerifier('recaptcha-container');
@@ -50,23 +53,30 @@ const VerifyPhoneComponent = ({ phoneNumber, step }) => {
 
     return (
         <div>
-            <h2> SMS Verification</h2>
+            <Row style={{ marginTop: "-20px" }}>
+                <Col md={{ span: 4, offset: 4 }} style={{ display: "flex" }}>
+                    <img src={logoImage} alt="logo" style={{ marginTop: "15px" }} />
+                </Col>
+            </Row>
+            <p style={{ marginTop: "10px", fontStyle: "italic" }} >Step 3/3</p>
+            <p className="text-white " style={{ marginTop: "-20px", fontStyle: "italic" }}>Phone Verification</p>
             {isCaptchaVisible && (
                 <div>
                     <div id="recaptcha-container"></div>
                 </div>
             )}
-            {!isCaptchaVisible && (
-                <div>
-                    <div>
-                        <label className="text-white">OTP:</label>
-                        <input type="text" value={otp} onChange={(e) => setOtp(e.target.value)} />
-                    </div>
-                    <div>
-                        <button onClick={handleVerifyOtp} className="btn btn-primary">Verify OTP</button>
-                    </div>
-                </div>
-            )}
+            {/* {!isCaptchaVisible && ( */}
+
+            <div className="d-flex flex-column ">
+                <p className="text-white mb-3">Enter Verification Code: </p>
+                <MDBInput wrapperClass='mb-4 w-100' type='text' size="lg" placeholder='OTP...' style={{ color: 'white' }} value={otp} onChange={(e) => setOtp(e.target.value)} />
+
+                <MDBBtn size='lg' className="mx-auto" style={{ backgroundColor: "#1FDF64" }} onClick={handleVerifyOtp} >
+                    Verify OTP
+                </MDBBtn>
+            </div>
+
+            {/* )} */}
         </div>
     );
 };
