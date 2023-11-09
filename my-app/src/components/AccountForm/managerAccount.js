e15d7247cc777ba0a1378f8b7e1985355578ef26
Minh Duc <ngominhduc100@gmail.com>
2023-11-10 02:53:19 +0700

    add paging for admin manager user

--- my-app/src/components/AccountForm/managerAccount.js
+++ my-app/src/components/AccountForm/managerAccount.js
@@ -6,33 +6,41 @@ import "./style.css";
 export default function UserManager() {
     const [users, setUsers] = useState([]);
     const [search, setSearch] = useState('');
-    const [total, setTotal] = useState(0);
-    const [discountTotal, setDiscountTotal] = useState(0);
-
-    let { userId } = useParams();
+    const [currentPage, setCurrentPage] = useState(1);
+    const itemsPerPage = 5;
+    const [totalPages, setTotalPages] = useState(1);
 
     useEffect(() => {
         fetch(`http://localhost:9999/users`)
             .then(res => res.json())
             .then(res => {
                 setUsers(res);
+                setTotalPages(calculateTotalPages());
             });
-    }, []);
+    }, [currentPage]);
 
-    function handleSearch(search) {
+
+
+    const calculateTotalPages = () => {
+        return Math.ceil(users.length / itemsPerPage);
+    };
+
+    const startIndex = (currentPage - 1) * itemsPerPage;
+    const endIndex = Math.min(currentPage * itemsPerPage, users.length);
+
+    const displayedUsers = users.slice(startIndex, endIndex);
+
+    const handleSearch = (search) => {
         setSearch(search);
-    }
-
-    function handleQuantity(user, e) {
-        console.log(e.target.value);
-    }
+        setCurrentPage(1); // Reset to the first page when searching
+    };
 
     return (
         <Container>
             <div className="col text-center">User Manager</div>
             <div className="row">
                 <div className="col text-center">
-                    <input type="text" placeholder="Enter Name to search..." onChange={e => { handleSearch(e.target.value) }} />
+                    <input type="text" placeholder="Enter Name to search..." onChange={e => handleSearch(e.target.value)} />
                 </div>
             </div>
             <div className="row">
@@ -49,12 +57,12 @@ export default function UserManager() {
                         </tr>
                     </thead>
                     <tbody>
-                        {users.map(user => (
+                        {displayedUsers.map(user => (
                             <tr key={user.id}>
                                 <td>{user.id}</td>
                                 <td>{user.email}</td>
                                 <td>{user.name}</td>
-                                <td>{user.gender}</td>
+                                <td>{user.gender === 1 ? 'Male' : 'Female'}</td>
                                 <td>{user.birthday}</td>
                                 <td>{user.role}</td>
                                 <td>
@@ -65,6 +73,46 @@ export default function UserManager() {
                     </tbody>
                 </Table>
             </div>
+            <div className="row">
+                <div className="col text-center">
+                    <p>
+                        Page {currentPage} of {totalPages}
+                    </p>
+                    <nav>
+                        <ul className="pagination">
+                            <li className={`page-item ${currentPage === 1 ? 'disabled' : ''}`}>
+                                <button
+                                    className="page-link"
+                                    onClick={() => setCurrentPage(currentPage - 1)}
+                                >
+                                    Previous
+                                </button>
+                            </li>
+                            {Array.from({ length: totalPages }, (_, index) => (
+                                <li
+                                    key={index + 1}
+                                    className={`page-item ${currentPage === index + 1 ? 'active' : ''}`}
+                                >
+                                    <button
+                                        className="page-link"
+                                        onClick={() => setCurrentPage(index + 1)}
+                                    >
+                                        {index + 1}
+                                    </button>
+                                </li>
+                            ))}
+                            <li className={`page-item ${currentPage === totalPages ? 'disabled' : ''}`}>
+                                <button
+                                    className="page-link"
+                                    onClick={() => setCurrentPage(currentPage + 1)}
+                                >
+                                    Next
+                                </button>
+                            </li>
+                        </ul>
+                    </nav>
+                </div>
+            </div>
         </Container>
-    )
+    );
 }
